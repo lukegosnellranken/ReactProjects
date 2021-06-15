6e6c76bf09ae5f6a1f5aeb717e77bec339d19da0
Gosnell <gosnellwebdesign@gmail.com>
2021-06-14 22:49:25 -0500

    Chart added

--- DynamicExpenseList/src/components/Expenses/Expenses.js
+++ DynamicExpenseList/src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -20,7 +21,8 @@ const Expenses = (props) => {
 
     return (<div>
         <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses}/>
         {/* <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}> </ExpenseItem>
         <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}> </ExpenseItem> 

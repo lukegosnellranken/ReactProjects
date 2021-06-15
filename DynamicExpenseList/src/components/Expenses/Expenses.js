f722deece0c42012679c94107c355efca7b11535
Gosnell <gosnellwebdesign@gmail.com>
2021-06-14 22:03:09 -0500

    List rendering adjustments

--- DynamicExpenseList/src/components/Expenses/Expenses.js
+++ DynamicExpenseList/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -12,10 +12,16 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    
+
     return (<div>
         <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-        {props.items.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)}
+        <ExpensesList items={filteredExpenses}/>
         {/* <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}> </ExpenseItem>
         <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}> </ExpenseItem> 
         <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}> </ExpenseItem> 

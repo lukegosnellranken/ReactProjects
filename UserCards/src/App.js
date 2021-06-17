fb35a515cafe47b95b0c610b1628bdd2a33305c7
Gosnell <gosnellwebdesign@gmail.com>
2021-06-16 23:01:51 -0500

    UserCards project

---
+++ UserCards/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import AddUser from './components/Users/AddUsers';
+
+
+function App() {
+  return (
+    <div>
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
